0f3738322e917f5f7831b775b26cfa7c2de9ed7f
JasonPedder <158420747+JasonPedder@users.noreply.github.com>
2025-09-15 22:19:51 -0500

    Used npm install npm install react-datepicker and npm install date-fns to change the date field into an actual date field. Additionally, added functionality for the match volunteers button and updated the modal for editing and creating events to handle the new date field in volunteer matching. Lastly, had the join events button in the events page be toggle-able to let the user know if they can either join or leave event, later on this would be stored in the back end or database indicating if that user is already a part of the event or not

--- frontend/src/app/events/page.jsx
+++ frontend/src/app/events/page.jsx
@@ -6,7 +6,10 @@ const sampleEvents = [
   {
     id: 1,
     title: "Traffic Director",
-    date: "9/29/25",
+    date: {
+      start: new Date(2025, 9, 29),
+      end: new Date(2025, 9, 29),
+    },
     urgency: "4",
     description: "Help needed to direct traffic for the home game against Rice University. Needed from 5pm to 9pm for directing traffic in and out.",
     image: "/images/events/traffic-director.jpg",
@@ -14,7 +17,10 @@ const sampleEvents = [
   {
     id: 2,
     title: "Soul Cleanup",
-    date: "10/1/25",
+    date: {
+      start: new Date(2025, 10, 1),
+      end: new Date(2025, 10, 1),
+    },
     urgency: "3",
     description: "Help needed cleaning the souls of the students who failed their OS exam in the agnes arnold auditorium because they didnt study semaphores enough",
     image: "/images/events/soul-cleanup.jpg",
@@ -22,7 +28,10 @@ const sampleEvents = [
   {
     id: 3,
     title: "Rebuild PGH",
-    date: "10/13/25-10/15/25",
+    date: {
+      start: new Date(2025, 10, 13),
+      end: new Date(2025, 10, 15),
+    },
     urgency: "2",
     description: "PGH is outdated and needs to be rebuilt. Easy task should take 2 days",
     image: "/images/events/rebuild-pgh.jpg",
@@ -30,7 +39,10 @@ const sampleEvents = [
   {
     id: 4,
     title: "Pop-Up Shop Vendor",
-    date: "10/16/25",
+    date: {
+      start: new Date(2025, 10, 16),
+      end: new Date(2025, 10, 16),
+    },
     urgency: "2",
     description: "Looking for vendors to setup tents in the green between PGH and the library, must sell allowed goods such as food, clothing, or jewelry. Must be friendly towards students and report all revenue and provide the campus 25% of total profits",
     image: "/images/events/popup-shop.jpg",
@@ -41,11 +53,20 @@ const sampleEvents = [
 
 function EventsPage() {
   const [count, setCount] = useState(0);
+  const [joinedEventIds, setJoinedEventIds] = useState([]);
+
+  const handleToggleJoin = (eventId) => {
+  setJoinedEventIds((prevIds) =>
+    prevIds.includes(eventId)
+      ? prevIds.filter(id => id !== eventId)
+      : [...prevIds, eventId]
+  );
+};
 
   return (
     <>
       <div className="max-h-[calc(100vh-12rem)] h-[calc(100vh-12rem)] mt-20">
-        <CardGrid events={sampleEvents}/>
+        <CardGrid events={sampleEvents} showButton={true} onToggleJoin={handleToggleJoin} joinedEventIds={joinedEventIds}/>
       </div>
     </>
   );

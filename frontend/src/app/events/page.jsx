7da7231200e5a431c9a09db36c3e43ea1222f9a6
JasonPedder <158420747+JasonPedder@users.noreply.github.com>
2025-10-08 22:29:26 -0500

    forgot variable lols

--- frontend/src/app/events/page.jsx
+++ frontend/src/app/events/page.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import CardGrid from '../components/events/CardGrid';
 
 function EventsPage() {
+  const API_URL = import.meta.env.VITE_API_URL;
   const [events, setEvents] = useState([]);
   const [joinedEventIds, setJoinedEventIds] = useState([]);
 

5e33f945e9716ba6e71720cf43e5712c971ccea5
ramseywiz <ramchandoklow@gmail.com>
2025-10-17 16:25:19 -0500

    added some extra auth tests

--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -6,8 +6,9 @@ describe("Auth routes", () =>{
     it("GET /api/auth should return 404 for unknown path", async () => {
         const res = await request(app).get('/api/auth/unknown');
         expect(res.statusCode).toBe(404);
-    })
+    });
 
+    // register endpoint
     it("POST /api/register should return 400 for missing fields", async () =>{
         const res = await request(app)
             .post("/api/auth/register")
@@ -18,5 +19,43 @@ describe("Auth routes", () =>{
             });
 
         expect(res.statusCode).toBe(400);
-    })
+    });
+
+    it("POST /api/register should return 200 on valid inputs", async () =>{
+        const res = await request(app)
+            .post("/api/auth/register")
+            .expect("Content-Type", /json/)
+            .send({
+                email: "xyz@amazon.com",
+                password: "hello",
+                userType: "admin"
+            })
+
+        expect(res.statusCode).toBe(200);
+    });
+
+    // login endpoint
+    it("POST /api/login should return 401 for invalid credentials", async () =>{
+        const res = await request(app)
+            .post("/api/auth/login")
+            .expect("Content-Type", /json/)
+            .send({
+                email: "fake@email.com",
+                password: "idontexist"
+            })
+
+        expect(res.statusCode).toBe(401);
+    });
+
+    it("POST /api/login should return 200 for valid match", async () =>{
+        const res = await request(app)
+            .post("/api/auth/login")
+            .expect("Content-Type", /json/)
+            .send({
+                email: "real@test.com",
+                password: "testpass"
+            })
+
+        expect(res.statusCode).toBe(200);
+    });
 })

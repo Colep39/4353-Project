aac96824b350c69dc38cf6e3d535cd627ff332e1
Cole Plagens <142531035+Colep39@users.noreply.github.com>
2025-10-31 16:22:54 -0500

    addded skills route and controller for fetching, user profile now successfully updates for user data and is enforced with data handling restrictions

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from './components/navbar/Navbar';
 import AuthProvider from './components/authProvider/authProvider';
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
       >
         <AuthProvider><Navbar /></AuthProvider>
         {children}
+        <Toaster position="top-right" />
       </body>
     </html>
   );
